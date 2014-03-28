242590b70776172a3751ceca0171cf4600b3661d
OBE Louise <lobe@p-212b-20.intra.gobelins.fr>
2014-03-28 10:23:08 +0100

    Remove edit and done buttons and modify column titles  enter the commit message for your changes. Lines starting

--- fluxRss/fluxRss/MasterViewController.m
+++ fluxRss/fluxRss/MasterViewController.m
@@ -29,11 +29,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-    self.navigationItem.leftBarButtonItem = self.editButtonItem;
 
-    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-    self.navigationItem.rightBarButtonItem = addButton;
     self.detailViewController = (DetailViewController *)[[self.splitViewController.viewControllers lastObject] topViewController];
     
     parse = [[ModelArticle alloc] init];
@@ -80,7 +76,7 @@
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
 {
     // Return NO if you do not want the specified item to be editable.
-    return YES;
+    return  NO;
 }
 
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
